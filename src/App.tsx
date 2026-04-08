d48b8535fe8a6c0f79063f061c8e09a304f1dc9c
hakanonlinex04-wq <hakanonlinex04@gmail.com>
2026-04-08 16:15:08 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { MessageCircle } from 'lucide-react';
